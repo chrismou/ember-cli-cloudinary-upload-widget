13dcf51b444453ca0c709b6660f986ef14fa242f
Chris Chrisostomou <dev@mou.me>
2019-07-08 15:28:21 +0100

    script import & predicted package name

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  contentFor(type, config) {
+    let pluginConfig = config.cloudinaryUploadWidget || {};
+
+    let excludeLibrary = pluginConfig.excludeLibrary;
+
+    if (type === 'body' && !excludeLibrary) {
+      return '<script type="text/javascript" src="https://widget.cloudinary.com/v2.0/global/all.js"></script>';
+    }
+  }
 };
